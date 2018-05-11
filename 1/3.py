e865996243814878387dfddec9e78dcab722f4e8
mohsenjalalian <jalalianmohsen@gmail.com>
2018-05-11 17:26:36 +0430

    session 1 practice 2

---
+++ 1/3.py
@@ -0,0 +1,4 @@
+hazine_zamin_metri = 10
+masahate_zamin = 20
+hazineye_zamin = hazine_zamin_metri * masahate_zamin
+print( hazineye_zamin )
---
+++ 1/4.py
@@ -0,0 +1,5 @@
+hazine_zamin_metri = 10
+masahate_zamin = 20
+hazineye_zamin = hazine_zamin_metri * masahate_zamin
+masahate_zamin = 30
+print( hazineye_zamin )
