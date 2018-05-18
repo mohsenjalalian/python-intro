baf6a2895c5828eb6a9d5fb48d70b08b3574a2bd
mohsenjalalian <jalalianmohsen@gmail.com>
2018-05-18 15:55:42 +0430

    directory structure

---
+++ 1/quiz/10.py
@@ -0,0 +1,9 @@
+a = 3
+b = 4
+c = 5
+
+print(a < 5)
+print(b < 4)
+print((a > 1) and (b > 1))
+print(c > a)
+print(b > a+1)
---
+++ 1/quiz/11.py
@@ -0,0 +1,7 @@
+age = 13
+if (0 <= age) and (age < 6):
+    print("khordsal")
+elif (6 <= age) and (age < 10):
+    print("koodak")
+elif (10 <= age) and (age < 14):
+    print("nojavan")
