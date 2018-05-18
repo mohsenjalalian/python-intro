1ce58c153e80948ce1349cf46a2e7f939d91d193
mohsenjalalian <jalalianmohsen@gmail.com>
2018-05-18 12:09:30 +0430

    5,6,7

---
+++ 1/5.py
@@ -0,0 +1,4 @@
+print(2 ** 3)
+print(33 // 17)
+print(11 % 3)
+print( 'a'*1 + 'b'*2 + 'c'*3 )
---
+++ 1/6.py
@@ -0,0 +1,4 @@
+var = 23
+print( str(var % 10) + str(var // 10) )
+var = 234
+print(( var // 10 ) % 10)
